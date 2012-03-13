d88d6a4c4147f50c2d631b4af11471bc71428f17
Julien Cloute <cloute_j@epita.fr>
2012-03-13 04:07:46 +0100

    Added empty IMap interface. Removed Ptr[List][Array]: replaced by encapsulated ptr typedef.

---
+++ YAPOG/include/YAPOG/World/Map/IMap.hpp
@@ -0,0 +1,15 @@
+#ifndef YAPOG_IMAP_HPP
+# define YAPOG_IMAP_HPP
+
+# include "YAPOG/Export.hpp"
+# include "YAPOG/World/Map/Physics/ICollidable.hpp"
+
+namespace yap
+{
+  struct YAPOG_LIB IMap
+  {
+      virtual ~IMap () {}
+  };
+} /// namespace yap
+
+#endif /// !YAPOG_IMAP_HPP
--- YAPOG/include/YAPOG/World/Map/Physics/ICollidable.hpp
+++ YAPOG/include/YAPOG/World/Map/Physics/ICollidable.hpp
@@ -1,18 +1,22 @@
 #ifndef YAPOG_ICOLLIDABLE_HPP
 # define YAPOG_ICOLLIDABLE_HPP
 
+# include "YAPOG/Export.hpp"
 # include "YAPOG/Vector2.hpp"
 
 namespace yap
 {
-  struct ICollidable
+  struct YAPOG_LIB ICollidable
   {
+      typedef std::shared_ptr<ICollidable> PtrType;
+
       virtual ~ICollidable () {}
 
       virtual const Vector2& GetPosition () const = 0;
       virtual const Vector2& GetSize () const = 0;
 
       virtual void Move (const Vector2& offset) = 0;
+      virtual void Scale (const Vector2& factor) = 0;
   };
 } /// namespace yap
 
