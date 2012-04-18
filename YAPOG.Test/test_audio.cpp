14486b46866091eafa84f4a6980fe7f4c9484bd1
Jonathan JARRI <noxalus@gmail.com>
2012-04-18 16:01:08 +0200

    Fix the build error with the dynamic library files copy o Windows.

--- YAPOG.Test/test_audio.cpp
+++ YAPOG.Test/test_audio.cpp
@@ -1,37 +1,52 @@
- #include <SFML/Audio.hpp>
- #include <SFML/Graphics.hpp>
- 
- int main()
- {
-     // Create the main window
-     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
- 
-     // Load a music to play
-     sf::Music music;
-     if (!music.openFromFile("wild_pokemon_battle_short.ogg"))
-         return EXIT_FAILURE;
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-     // Play the music
-     music.play();
- 
-     // Start the game loop
-     while (window.isOpen())
-     {
-         // Process events
-         sf::Event event;
-         while (window.pollEvent(event))
-         {
-             // Close window : exit
-             if (event.type == sf::Event::Closed)
-                 window.close();
-         }
- 
-         // Clear screen
-         window.clear();
- 
-         // Update the window
-         window.display();
-     }
- 
-     return EXIT_SUCCESS;
- }
+int main()
+{
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+	// Load a music to play
+	sf::Music music;
+	if (!music.openFromFile("trainer_battle.ogg"))
+	{
+		getchar();
+		return EXIT_FAILURE;
+	}
+
+	//music.setPlayingOffset(sf::Time(sf::seconds(37.f)));
+
+	music.setPlayingOffset(sf::Time(sf::seconds(89.75f)));
+	// Play the music
+	music.play();
+
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window : exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		std::cout << music.getPlayingOffset().asSeconds() << std::endl;
+
+		
+		if (music.getPlayingOffset() >= sf::Time(sf::seconds(90.5f)))
+		{
+			music.setPlayingOffset(sf::Time(sf::seconds(12.75f)));
+		}
+		
+		// Clear screen
+		window.clear();
+
+		// Update the window
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+}
