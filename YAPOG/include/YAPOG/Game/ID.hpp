5a04a4b4d929735fce369b4e7e8ea2380fd3a1b3
Jonathan JARRI <noxalus@gmail.com>
2012-04-25 16:29:32 +0200

    Merged the game-engine branch with master.

--- YAPOG/include/YAPOG/Game/ID.hpp
+++ YAPOG/include/YAPOG/Game/ID.hpp
@@ -21,22 +21,12 @@ namespace yap
       const ValueType& GetValue () const;
       void SetValue (ValueType value);
 
-<<<<<<< HEAD
       bool operator== (const ID& right) const;
       bool operator!= (const ID& right) const;
       bool operator> (const ID& right) const;
       bool operator< (const ID& right) const;
       bool operator>= (const ID& right) const;
       bool operator<= (const ID& right) const;
-
-=======
-      bool operator== (const ID& right);
-      bool operator!= (const ID& right);
-      bool operator> (const ID& right);
-      bool operator< (const ID& right);
-      bool operator>= (const ID& right);
-      bool operator<= (const ID& right);
->>>>>>> 63750bb27bcf3a759bac1f1ae9b602fd642e4c15
     private:
 
       static const ValueType DEFAULT_VALUE;
