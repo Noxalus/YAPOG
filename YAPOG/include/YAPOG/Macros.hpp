5e338b4d89415f56b9cb568eae10aa9d0705c2f3
Julien Cloute <cloute_j@epita.fr>
2012-04-27 15:28:03 +0200

    Fixed in Macros.hpp YAPOG_LIB definition for Linux. Fixed calls to XmlReader::ReadNodes that produced VC++ internal error. Added condition for DLLEXPORT macro in CMakeLists.txt.

--- YAPOG/include/YAPOG/Macros.hpp
+++ YAPOG/include/YAPOG/Macros.hpp
@@ -12,16 +12,16 @@
   TYPE& operator= (const TYPE&)
 
 /// WIN32 dll export
-#ifdef _MSC_VER
-# define YAPOG_LIB_EXPORT __declspec(dllexport)
-# define YAPOG_LIB_IMPORT __declspec(dllimport)
-# ifdef DLLEXPORT
-#  define YAPOG_LIB YAPOG_LIB_EXPORT
+# ifdef _MSC_VER
+#  define YAPOG_LIB_EXPORT __declspec(dllexport)
+#  define YAPOG_LIB_IMPORT __declspec(dllimport)
+#  ifdef DLLEXPORT
+#   define YAPOG_LIB YAPOG_LIB_EXPORT
+#  else
+#   define YAPOG_LIB YAPOG_LIB_IMPORT
+#  endif  // DLLEXPORT
 # else
-#  define YAPOG_LIB YAPOG_LIB_IMPORT
-# endif  // DLLEXPORT
-#else
-# define DLLDECL
-#endif // _MSC_VER
+#  define YAPOG_LIB
+# endif // _MSC_VER
 
 #endif // YAPOG_MACROS_HPP
