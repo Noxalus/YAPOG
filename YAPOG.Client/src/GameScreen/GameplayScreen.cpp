20cfe18051fbb19c0a376bc7d98839f4dceb78b0
PsykoBlack Metalleux <PBM764927@gmail.com>
2012-06-08 18:22:37 +0200

    Fix MultiLabel Moving And AutoScrolling

--- YAPOG.Client/src/GameScreen/GameplayScreen.cpp
+++ YAPOG.Client/src/GameScreen/GameplayScreen.cpp
@@ -78,10 +78,28 @@ namespace ycl
 
     worldCamera_.Scale (DEFAULT_WORLD_CAMERA_DEZOOM_FACTOR);
 
+    
     /// @warning Commented.
-    // ChatWidget* chat = new ChatWidget ();
-    // chat->Init ();
-    // guiManager_->AddChild (*chat);
+    ChatWidget* chat = new ChatWidget ();
+    chat->Init ();
+    guiManager_->AddChild (*chat);
+    
+    /*
+    yap::MultiLabelWidget* labels =
+      new yap::MultiLabelWidget (yap::Padding (5, 5, 5, 5), yap::Padding (0, 0, 0, 0), false);
+    labels->SetSize (yap::Vector2 (242, 96));
+    yap::HorizontalLayout* diaLayout = 
+      new yap::HorizontalLayout (yap::Padding (5, 5, 0, 0), yap::Padding (0, 0, 0, 0), true);    
+    diaLayout->SetBorder (*new yap::WidgetBorder ("Test/red.png"));    
+    diaLayout->AddChild (*labels);
+
+    diaLayout->SetPosition (yap::Vector2 (512, 256));
+    for (int i = 0; i < 10; i++)
+    {
+      labels->AddText ("Test" + yap::StringHelper::ToString (i), 12);
+    }
+    guiManager_->AddChild (*diaLayout);
+    */
   }
 
   const yap::ScreenType& GameplayScreen::HandleRun (
@@ -101,64 +119,64 @@ namespace ycl
   bool GameplayScreen::HandleOnEvent (const yap::GuiEvent& guiEvent)
   {
     if (gameInputManager_.GameInputIsActivated (
-          yap::GameInputType::Down,
-          guiEvent))
+      yap::GameInputType::Down,
+      guiEvent))
     {
       moveController_.EnableDirection (yap::Direction::South);
       return true;
     }
 
     if (gameInputManager_.GameInputIsActivated (
-          yap::GameInputType::Up,
-          guiEvent))
+      yap::GameInputType::Up,
+      guiEvent))
     {
       moveController_.EnableDirection (yap::Direction::North);
       return true;
     }
 
     if (gameInputManager_.GameInputIsActivated (
-          yap::GameInputType::Left,
-          guiEvent))
+      yap::GameInputType::Left,
+      guiEvent))
     {
       moveController_.EnableDirection (yap::Direction::West);
       return true;
     }
 
     if (gameInputManager_.GameInputIsActivated (
-          yap::GameInputType::Right,
-          guiEvent))
+      yap::GameInputType::Right,
+      guiEvent))
     {
       moveController_.EnableDirection (yap::Direction::East);
       return true;
     }
 
     if (gameInputManager_.GameInputIsDeactivated (
-          yap::GameInputType::Down,
-          guiEvent))
+      yap::GameInputType::Down,
+      guiEvent))
     {
       moveController_.DisableDirection (yap::Direction::South);
       return true;
     }
 
     if (gameInputManager_.GameInputIsDeactivated (
-          yap::GameInputType::Up,
-          guiEvent))
+      yap::GameInputType::Up,
+      guiEvent))
     {
       moveController_.DisableDirection (yap::Direction::North);
       return true;
     }
 
     if (gameInputManager_.GameInputIsDeactivated (
-          yap::GameInputType::Left,
-          guiEvent))
+      yap::GameInputType::Left,
+      guiEvent))
     {
       moveController_.DisableDirection (yap::Direction::West);
       return true;
     }
 
     if (gameInputManager_.GameInputIsDeactivated (
-          yap::GameInputType::Right,
-          guiEvent))
+      yap::GameInputType::Right,
+      guiEvent))
     {
       moveController_.DisableDirection (yap::Direction::East);
       return true;
@@ -176,8 +194,8 @@ namespace ycl
   {
     cameraController_.SetBounds (
       yap::FloatRect (
-        yap::Vector2 (),
-        map.GetSize ()));
+      yap::Vector2 (),
+      map.GetSize ()));
 
     /// @warning Tests.
     /// @todo Remove from here.
@@ -190,34 +208,34 @@ namespace ycl
     yap::MapEvent* event = new yap::MapEvent ();
     event->AddBoundingBox (
       new yap::BoundingBox (
-        yap::Vector2 (-30, -30), yap::Vector2 (60, 60), 0, 1));
+      yap::Vector2 (-30, -30), yap::Vector2 (60, 60), 0, 1));
 
     event->AddAction (
       new yap::AnyMapEventAction (yap::MapEventActionType::Leave,
-        [this] (yap::MapEventArgs& args)
-        {
-          logger_.LogLine ("OUT");
+      [this] (yap::MapEventArgs& args)
+    {
+      logger_.LogLine ("OUT");
 
-          return true;
-        }));
+      return true;
+    }));
 
     event->AddAction (
       new yap::AnyMapEventAction (yap::MapEventActionType::Enter,
-        [this] (yap::MapEventArgs& args)
-        {
-          logger_.LogLine ("IN");
+      [this] (yap::MapEventArgs& args)
+    {
+      logger_.LogLine ("IN");
 
-          return true;
-        }));
+      return true;
+    }));
 
     event->AddAction (
       new yap::AnyMapEventAction (yap::MapEventActionType::In,
-        [this] (yap::MapEventArgs& args)
-        {
-          logger_.LogLine ("UPDATE.....................");
+      [this] (yap::MapEventArgs& args)
+    {
+      logger_.LogLine ("UPDATE.....................");
 
-          return true;
-        }));
+      return true;
+    }));
 
     npc1_->AddEvent (event);
   }
@@ -235,7 +253,7 @@ namespace ycl
     /// @todo Remove from here.
     player_->AddTriggerBoundingBox (
       new yap::BoundingBox (
-        yap::Vector2 (-20, -20), yap::Vector2 (40, 40), 0, 1));
+      yap::Vector2 (-20, -20), yap::Vector2 (40, 40), 0, 1));
   }
 
   void GameplayScreen::UpdatePlayer (const yap::Time& dt)
