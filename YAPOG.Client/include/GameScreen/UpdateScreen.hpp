cb836c5e7da7b1f2486444e0727995a545e6478d
Franck LY <franck.ly@epita.fr>
2012-06-13 21:47:56 +0200

    UpdaterScreen finished.

--- YAPOG.Client/include/GameScreen/UpdateScreen.hpp
+++ YAPOG.Client/include/GameScreen/UpdateScreen.hpp
@@ -2,6 +2,12 @@
 # define YAPOG_CLIENT_UPDATESCREEN_HPP
 
 # include "YAPOG/Macros.hpp"
+# include "YAPOG/System/IntTypes.hpp"
+# include "YAPOG/System/Hash/FileChecker.hpp"
+# include "YAPOG/Graphics/Gui/Label.hpp"
+# include "YAPOG/Graphics/Gui/PictureBox.hpp"
+# include "YAPOG/Graphics/Gui/VerticalLayout.hpp"
+# include "YAPOG/Graphics/Gui/HorizontalLayout.hpp"
 
 # include <boost/filesystem.hpp>
 # include "GameScreen/BaseScreen.hpp"
@@ -27,6 +33,19 @@ namespace ycl
 
     private:
       void Update ();
+      void UpdateContentSize (int value);
+
+      yap::UInt16             lastlength_;
+      yap::FileChecker*       fc_;
+      yap::Label              fileName_;
+      yap::Label              filePercentage_;
+      yap::Label              fileDownloaded_;
+      yap::Label              totalFile_;
+      yap::PictureBox*        backgroundbox_;
+      yap::PictureBox*        foregroundbox_;
+      yap::HorizontalLayout*  firstLayout_;
+      yap::HorizontalLayout*  secondLayout_;
+      yap::VerticalLayout*    bigLayout_;
 
       static const yap::ScreenType DEFAULT_NAME;
   };
